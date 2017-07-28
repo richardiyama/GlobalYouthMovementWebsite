9d869dc6afe7e725d8a2686b617e4a12ef6a5ee1
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-28 13:20:39 +0100

    Update CommentsController.cs

--- GlobalYouthWeb/Controllers/CommentsController.cs
+++ GlobalYouthWeb/Controllers/CommentsController.cs
@@ -14,6 +14,7 @@ namespace GlobalYouthWeb.Controllers
     {
         private GlobalYouthWebContextDB db = new GlobalYouthWebContextDB();
 
+        
         // GET: Comments
         public ActionResult Index()
         {
