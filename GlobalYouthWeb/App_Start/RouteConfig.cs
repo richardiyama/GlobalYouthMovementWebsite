66e6575023c0e5257602a70bfb52720133fdae6a
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-28 13:17:15 +0100

    Update RouteConfig.cs

--- GlobalYouthWeb/App_Start/RouteConfig.cs
+++ GlobalYouthWeb/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace GlobalYouthWeb
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+           
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
